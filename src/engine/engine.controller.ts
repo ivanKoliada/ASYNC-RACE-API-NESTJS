979747ee556cfe4a5354c7fe5347d353f6dc9352
Ivan Koliada <ivanKoliada69@gmail.com>
2023-01-11 12:37:08 +0300

    feat: engine controller

---
+++ src/engine/engine.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Controller,
+  Header,
+  HttpException,
+  HttpStatus,
+  Patch,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { GarageService } from 'src/garage/garage.service';
+import { SwitchEngineDto } from './engine.dto';
+import { EngineService } from './engine.service';
+
+@Controller('engine')
+export class EngineController {
+  constructor(
+    private readonly garageService: GarageService,
+    private readonly engineService: EngineService,
+  ) {}
+
+  @Patch()
+  @Header('Content-Type', 'application/json')
+  async switchEngine(
+    @Query() switchEngineDto: SwitchEngineDto,
+    @Res() res: Response,
+  ) {
+    const car = await this.garageService.getCar(switchEngineDto.id);
+
+    if (car) {
+      return await this.engineService.switchEngine(switchEngineDto, res);
+    }
+
+    throw new HttpException(
+      'Car with such id was not found in the garage.',
+      HttpStatus.NOT_FOUND,
+    );
+  }
+}
