72df570a4d389fad7d7e7c58c829d74d07498c2f
Ivan Koliada <ivanKoliada69@gmail.com>
2023-01-11 11:23:23 +0300

    fix: id type number

--- src/engine/engine.dto.ts
+++ src/engine/engine.dto.ts
@@ -1,7 +1,9 @@
+import { Type } from 'class-transformer';
 import { IsIn, IsNumber } from 'class-validator';
 
 export class SwitchEngineDto {
   @IsNumber()
+  @Type(() => Number)
   id: number;
 
   @IsIn(['started', 'stopped', 'drive'])
